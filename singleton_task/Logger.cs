0c080795105fb309aff676ba54cbb09ec6319f7c
yazot <yazot@PC-RABOTYAGA>
2023-10-02 23:55:46 +0300

    Add project files.

---
+++ singleton_task/Logger.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace singleton_task
+{
+    public class Logger
+    {
+        private static Logger instance = new Logger(); // static instanace which is needed in case if only one instance is created
+
+        private Logger() { } // prevent direct instantiation, can't be initiated from outside
+        public static Logger Instance { get { return instance; } } //access to the instance
+
+        public void LogMess(string i) //the methoв to print the message
+        {
+            Console.WriteLine(i);
+        }
+      
+
+    }
+}
---
+++ singleton_task/Program.cs
@@ -0,0 +1,10 @@
+﻿using singleton_task;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Logger.Instance.LogMess("Hello, this is a new log message for You.");// logger access to print messages
+        Console.ReadKey();
+    }
+}
